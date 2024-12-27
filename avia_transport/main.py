8628704c226ac21144e546cb11ad010a722cd96d
devSparta <31nikita31@gmail.com>
2024-12-27 17:12:47 +0300

    release

--- avia_transport/main.py
+++ avia_transport/main.py
@@ -1,6 +1,7 @@
 from routers import app
-
+from models import Flight, Aviacompany, Ticket, Airplane, Client, Users, db
 
 if __name__ == '__main__':
     app.run(debug=True)
-
+    with db:
+        db.create_tables([Flight, Aviacompany, Ticket, Airplane, Client, Users])
